eff481da39bfd9b387a3e6285658a3073e2c637c
dhay salih <dhaysalih18@gmail.com>
2023-02-13 01:28:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="main-container">
+                <h1 className="main-header">
+                    <span className="h1-main">
+                         &lt;h1&gt;
+                    </span> i'm <span className="main-name">dhay salih</span><span className="h1-main"> &lt;/h1&gt;</span>
+
+                </h1>
+                <div className="secondary-container">
+                    <span className="span-main">&lt;span&gt;</span><h2 className="secondary-header">a UIUX designer and a front-end developer </h2> <span className="span-main">&lt;/span&gt;</span>
+                </div>
+            </div>
+
+            <div className="button">
+                 contact -me
+            </div>
+        </div>
+
+
+    )
+        ;
 }
 
 export default App;
